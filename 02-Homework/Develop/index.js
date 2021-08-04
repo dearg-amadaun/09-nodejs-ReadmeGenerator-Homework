04449a8dc9607dfdf343e3eea3ecd468699b90eb
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-08-04 16:59:10 -0400

    init commit

---
+++ 02-Homework/Develop/index.js
@@ -0,0 +1,75 @@
+// TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const util = require("util");
+const generateMarkdown = require("./util/generateMarkdown");
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+    type: "input",
+    message: "Please enter a project title.",
+    name: "title"
+}, {
+    type: "input",
+    message: "Please provide a short description of your project.",
+    name: "description"
+},  {
+    type: "input",
+    message: "Please provide installation instructions for your project.",
+    name:"installation"
+}, {
+    type: "input",
+    message: "Please provide instructions for the use of your project.",
+    name: "usage"
+}, {
+    type: "checkbox",
+    message: "Please choose your license.",
+    choices: [
+        "MIT",
+        "GNU GPLv3",
+        "Apache License 2.0"
+    ],
+    name: "license"
+}, {
+    type: "input",
+    message: "Please list collaborators, third party assets, and tutorials used with links.",
+    name: "contributors"
+}, {
+    type: "input",
+    message: "Please list commands needed to test this program.",
+    name: "tests"
+}, {
+    type: "input",
+    message: "Please enter your GitHub username",
+    name: "username" //place under questions heading
+}, {
+    type: "input",
+    message: "Please enter your e-mail address",
+    name: "e-mail" //place under questions heading
+},
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+
+    fs.writeToFile(fileName, data, function(err) {
+        if (err) { return console.log(err) 
+         } else { 
+        console.log("success")}
+        
+        console.log(fileName);
+    })
+};
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(function(data) {
+        writeToFile("README.md", generateMarkdown(data));
+        console.log(data)
+    })
+}
+
+// Function call to initialize app
+init();
---
+++ 02-Homework/Develop/utils/generateMarkdown.js
@@ -0,0 +1,55 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if(license != "none") {
+    return ""
+  }
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+          ## Table of Contents
+          *[Description](#description)
+          *[installation](#installation)
+          *[Usage](#usage)
+          *[License](#license)
+          *[Contributors](#contributors)
+          *[Tests](#tests)
+          *[Questions](#questions)
+
+          ### Description
+          ${response.description}
+
+          ### Installation
+          ${response.installation}
+
+          ### Usage
+          ${response.usage}
+
+          ###License
+          ${response.license}
+
+          ###Contributors
+          ${response.contributors}
+
+          ###Tests
+          ${response.tests}
+
+          ###Questions
+          Please direct any questions or comments to
+          ${response.username}
+          ${response.e-mail}
+
+`;
+}
+
+module.exports = generateMarkdown;
