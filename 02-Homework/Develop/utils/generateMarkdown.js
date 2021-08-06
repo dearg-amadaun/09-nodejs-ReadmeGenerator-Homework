fbb5549d8a0914e669d505cdb594977280f7a79b
dearg-amadaun <83721789+dearg-amadaun@users.noreply.github.com>
2021-08-05 21:54:04 -0400

    fix markdown language

--- 02-Homework/Develop/utils/generateMarkdown.js
+++ 02-Homework/Develop/utils/generateMarkdown.js
@@ -36,45 +36,46 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `${renderLicenseBadge(data.license)}
+  return `
+  ${renderLicenseBadge(data.license)}
   
-          #${data.title}
+  # ${data.title}
 
-          ## Table of Contents
-          *[Description](#Description)
-          *[Installation](#Installation)
-          *[Usage](#Usage)
-          *[License](#License)
-          *[Contributors](#Contributors)
-          *[Tests](#Tests)
-          *[Questions](#Questions)
-          
-          ##License
-          ${data.license}
-          ${renderLicenseLink(data.license)}
-          
-          ##Description
-          ${data.description}
-          
-
-          ##Installation
-          ${data.installation}
-
-          ##Usage
-          ${data.usage}
-
-
-          ##Contributors
-          ${data.contributors}
-
-          ##Tests
-          ${data.tests}
-
-          ##Questions
-          Please direct any questions or comments to:
-          ${data.username}
-          ${data.mail}
-`; 
+  ## Table of Contents
+  * [Description](#Description)
+  * [Installation](#Installation)
+  * [Usage](#Usage)
+  * [License](#License)
+  * [Contributors](#Contributors)
+  * [Tests](#Tests)
+  * [Questions](#Questions)
+            
+  ## License
+  ${data.license}
+   ${renderLicenseLink(data.license)}
+            
+  ## Description
+  ${data.description}
+            
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  
+   ## Contributors
+  ${data.contributors}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  Please direct any questions or comments to:
+  ${data.username}
+  ${data.mail}
+  `; 
 }
 
 module.exports = generateMarkdown;
